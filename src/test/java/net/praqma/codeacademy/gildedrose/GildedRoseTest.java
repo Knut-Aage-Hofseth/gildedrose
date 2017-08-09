508ecc1fca9ce53416662c9692ff61162613e661
Knut-Aage-Hofseth <Knut-Aage-Hofseth@users.noreply.github.com>
2017-08-09 16:33:21 +0200

    Added comment

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-
+    /**
+    *Test for Gilded Rose
+    */
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
