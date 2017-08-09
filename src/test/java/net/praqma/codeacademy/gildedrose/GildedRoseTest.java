83ab315026777e4f4d8129216523befc7d282dc2
Knut-Aage-Hofseth <Knut-Aage-Hofseth@users.noreply.github.com>
2017-08-09 16:46:11 +0200

    second test changed to passing state

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -20,7 +20,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("basic", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foot", app.items[0].name);
+        assertEquals("basic", app.items[0].name);
     }
 
 }
